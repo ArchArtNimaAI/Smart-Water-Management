ec3a5b5e48d8e0bde8d15e153d54dc7edc40bb67
NimaShamandi <143939602+ArchArtNimaAI@users.noreply.github.com>
2024-10-22 00:36:32 -1200

    Update Comparison_Plot.py

--- Smart_Water_Consumption/Comparison_Plot.py
+++ Smart_Water_Consumption/Comparison_Plot.py
@@ -1,1 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+results = {
+    'Model': ['Random Forest', 'Gradient Boosting', 'Neural Network'],
+    'MAE': [mae, mae_gbr, mae_nn],
+    'RMSE': [rmse, rmse_gbr, rmse_nn]
+}
+results_df = pd.DataFrame(results)
+print(results_df)
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Model', y='MAE', data=results_df)
+plt.title('Mean Absolute Error (MAE) Comparison')
+plt.ylabel('MAE')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Model', y='RMSE', data=results_df)
+plt.title('Root Mean Squared Error (RMSE) Comparison')
+plt.ylabel('RMSE')
+plt.show()
