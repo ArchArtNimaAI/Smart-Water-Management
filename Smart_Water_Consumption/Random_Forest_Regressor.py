2ab461d12b6bf952c612473fe1f1fe9f549ec0ee
NimaShamandi <143939602+ArchArtNimaAI@users.noreply.github.com>
2024-10-22 00:32:00 -1200

    Create Random_Forest_Regressor.py

---
+++ Smart_Water_Consumption/Random_Forest_Regressor.py
@@ -0,0 +1,13 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+rf = RandomForestRegressor(n_estimators=100, random_state=42)
+
+rf.fit(X_train_scaled, y_train)
+y_pred = rf.predict(X_test_scaled)
+
+mae = mean_absolute_error(y_test, y_pred)
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+
+print(f"Mean Absolute Error (MAE): {mae}")
+print(f"Root Mean Square Error (RMSE): {rmse}")
